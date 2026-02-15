ad4f0496bbbfd937e40272c463a7031581b374e9
ztBAKUMAN <130461576+ztBAKUMAN@users.noreply.github.com>
2026-02-16 01:16:14 +0800

    Day19: Implementing asynchronous loggin using producer-consumer

--- Daily_Code/Phase_01_ModernCpp/day19_AsyncLog/include/MyLogger.hpp
+++ Daily_Code/Phase_01_ModernCpp/day19_AsyncLog/include/MyLogger.hpp
@@ -34,6 +34,15 @@ public:
 
     ~MyLogger()
     {
+        {
+            std::lock_guard<std::mutex> lock(m_queueMutex);
+            m_exit = true;
+        }
+        m_cv.notify_all();
+        if(m_workerThread.joinable())
+        {
+            m_workerThread.join();
+        }
         // 析构时关闭文件
         if(m_logFile.is_open())
         {
@@ -63,70 +72,104 @@ public:
     template <typename... Args>
     void Log(LogLevel level, const char *filename, int line, Args... args)
     {
-        std::lock_guard<std::mutex> locker(m_mutex);
-
-        // 打印时间
-        auto nowTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
         std::stringstream ss;
+        // 当前时间
+        auto nowTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
         ss << "[" << std::put_time(std::localtime(&nowTime), "%H:%M:%S") << "]";
-        std::string timeString = ss.str();
 
-        // 打印日志等级，用不同颜色区分
-        // \033[34m表示Linux终端控制符
+        // 日志等级
         const char* levelString = "";
-        const char* colorString = "";
         switch (level)
         {
         case LogLevel::EM_DEBUG:
-            levelString = "[DEBUG]"; colorString = "\033[34m";
+            levelString = "[DEBUG]";
             break; // 蓝色
         case LogLevel::EM_INFO:
-            levelString = "[INFO]"; colorString = "\033[32m";
+            levelString = "[INFO]";
             break; // 绿色
         case LogLevel::EM_WARN:
-            levelString = "[WARN]"; colorString = "\033[33m";
+            levelString = "[WARN]";
             break; // 黄色
         case LogLevel::EM_ERROR:
-            levelString = "[ERROR]"; colorString = "\033[31m";
+            levelString = "[ERROR]";
             break; // 红色
         default:
             break;
         }
-    
-        std::cout << timeString << " " << colorString << levelString << "\033[0m ";
-        // 打印实际内容(递归打印剩余参数) C++17折叠表达式实现
-        ((std::cout << args << " "), ...);
-        // 打印文件和行号
-        std::cout << " (" << filename << ":" << line << ")" << std::endl;
+        // 等级信息
+        ss << levelString << " ";
+        // 实际内容
+        ((ss << args << " "), ...);
+        // 文件和行号
+        ss << " (" << filename << ":" << line << ")" << std::endl;
 
-        // 将日志写入文件
-        if(m_logFile.is_open())
+        // 控制台输出
+        std::cout << ss.str() << std::endl;
+
+        // 加入队列
         {
-            m_logFile << timeString << " " << levelString << " ";
-            ((m_logFile << args << " "), ...);
-            m_logFile << " (" << filename << ":" << line << ")" << std::endl;
+            std::lock_guard<std::mutex> lock(m_queueMutex);
+            m_tasks.push(ss.str());
+        }
+        // 唤醒线程
+        m_cv.notify_one();
+    }
 
-            // *如果发生ERROR, 刷新缓冲区
-            if(level == LogLevel::EM_ERROR)
+private:
+    MyLogger() : m_exit(false)
+    {
+        open(defaulFile);
+        m_workerThread = std::thread(&MyLogger::processTask, this);
+    }
+    MyLogger(std::string fileName) : m_filename(fileName), m_exit(false)
+    {
+        open(m_filename);
+        m_workerThread = std::thread(&MyLogger::processTask, this);
+    }
+    MyLogger(const MyLogger &obj) = delete;
+    MyLogger &operator=(const MyLogger &obj) = delete;
+
+    void processTask()
+    {
+        // 后台线程写入日志
+        while(true)
+        {
+            std::string logMsg;
+            // 获取任务
             {
-                m_logFile.flush();
+                std::unique_lock<std::mutex> lock(m_queueMutex);
+                // 等待队列不为空或退出
+                m_cv.wait(lock, [this]{
+                    return !m_tasks.empty() || m_exit;
+                });
+
+                if(m_exit && m_tasks.empty())
+                {
+                    break;
+                }
+
+                // 取出任务
+                logMsg = std::move(m_tasks.front());
+                m_tasks.pop();
+            }
+            // 写日志
+            if(m_logFile.is_open())
+            {
+                m_logFile << logMsg << std::endl;
             }
         }
     }
 
 private:
-    MyLogger()
-    {
-        open(defaulFile);
-    }
-    MyLogger(const MyLogger &obj) = delete;
-    MyLogger &operator=(const MyLogger &obj) = delete;
-
     std::mutex m_mutex;         // 保护logFile
+    std::string m_filename;     // 文件名
     std::ofstream m_logFile;    // 日志文件
     // 异步成员
     std::mutex m_queueMutex;    // 保护日志队列
-    std::condition_variable m_cv;   // 条件变量(信号)
+    std::condition_variable m_cv;   // 条件变量(信号灯)
+    std::queue<std::string> m_tasks;// 日志队列
+    std::thread m_workerThread; // 写日志线程
+    bool m_exit;                // 是否退出
 };
 
 // 预处理将 __FILE__ 和 __LINE__ 自动替换为文件名和行号
